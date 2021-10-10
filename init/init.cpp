271020e643199ab2bac74d20918b59ef43f3fb19
Demon000 <demonsingur@gmail.com>
2021-10-10 16:14:41 +0100

    blossom: init: override dalvik heap config based on total RAM
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- init/init.cpp
+++ init/init.cpp
@@ -1,11 +1,16 @@
 /*
  * Copyright (C) 2021 crDroid Android Project
+ * Copyright (C) 2020 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <cstdlib>
 #include <fstream>
 #include <string>
+#include <string.h>
+#include <sys/sysinfo.h>
+#include <unistd.h>
 
 #include <android-base/properties.h>
 #include <libinit_variants.h>
@@ -14,6 +19,46 @@
 #include "vendor_init.h"
 
 using android::base::GetProperty;
+
+char const *heapstartsize;
+char const *heapgrowthlimit;
+char const *heapsize;
+char const *heapminfree;
+char const *heapmaxfree;
+char const *heaptargetutilization;
+
+void read_device_ram()
+{
+    struct sysinfo sys;
+
+    sysinfo(&sys);
+
+    if (sys.totalram >= 5ull * 1024 * 1024 * 1024) {
+        // from - phone-xhdpi-6144-dalvik-heap.mk
+        heapstartsize = "16m";
+        heapgrowthlimit = "256m";
+        heapsize = "512m";
+        heaptargetutilization = "0.5";
+        heapminfree = "8m";
+        heapmaxfree = "32m";
+    } else if (sys.totalram >= 3ull * 1024 * 1024 * 1024) {
+        // from - phone-xhdpi-4096-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "192m";
+        heapsize = "512m";
+        heaptargetutilization = "0.6";
+        heapminfree = "8m";
+        heapmaxfree = "16m";
+    } else {
+        // from - phone-xhdpi-2048-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "192m";
+        heapsize = "512m";
+        heaptargetutilization = "0.75";
+        heapminfree = "512k";
+        heapmaxfree = "8m";
+    }
+}
 
 bool check_device_has_fp() {
     std::ifstream ifs("/proc/bus/input/devices");
@@ -30,6 +75,15 @@ bool check_device_has_fp() {
 }
 
 void vendor_load_properties() {
+    read_device_ram();
+
+    property_override("dalvik.vm.heapstartsize", heapstartsize);
+    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    property_override("dalvik.vm.heapsize", heapsize);
+    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
+    property_override("dalvik.vm.heapminfree", heapminfree);
+    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
+
     std::string country = GetProperty("ro.boot.hwc", "");
     std::string name = GetProperty("ro.boot.hwname", "");
 
