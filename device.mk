31015183fd5d1748fa618c7fbf82544989f5f20b
Albert I <kras@raphielgang.org>
2021-11-02 21:47:53 +0530

    blossom: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc

--- device.mk
+++ device.mk
@@ -396,6 +396,8 @@ PRODUCT_COPY_FILES += \
 # Always preopt extracted APKs to prevent extracting out of the APK
 # for gms modules.
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 USE_DEX2OAT_DEBUG := false
 
 # Speed profile services and wifi-service to reduce RAM and storage
