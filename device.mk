e9779a3f4e852dffac45316c051351e1cc6a94f6
SamarV-121 <samarvispute121@pm.me>
2022-09-14 18:25:32 +0100

    blossom: Build OSS Lights AIDL from RM6785
    Co-authored-by: Demon000 <demonsingur@gmail.com>
    Co-authored-by: vasishath <vasishath@gmail.com>
    Co-authored-by: daniml3 <daniel@danielml.dev>
    
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- device.mk
+++ device.mk
@@ -81,6 +81,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.blossom
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1 \
