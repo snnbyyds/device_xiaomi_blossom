639b64118f3f19139e4753b75dd9c6c5463af3ba
Nolen Johnson <johnsonnolen@gmail.com>
2022-09-17 10:03:35 +0100

    blossom: Set sys.usb.ffs.aio_compat in recovery
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- device.mk
+++ device.mk
@@ -24,6 +24,11 @@ PRODUCT_PACKAGES += \
     fstab.mt6765_ramdisk \
     fstab.mt6762_ramdisk
 
+# Recovery
+PRODUCT_PACKAGES += \
+    init.recovery.mt6765.rc \
+    init.recovery.mt6762.rc
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1600
 TARGET_SCREEN_WIDTH := 720
---
+++ recovery/Android.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+# Recovery init configuration files
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.mt6765.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := root/init.recovery.mt6765.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.mt6762.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := root/init.recovery.mt6762.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
