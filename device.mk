e713856d2ebc047e6253d577e40c4a7dc330cfd4
Michael Bestas <mkbestas@lineageos.org>
2022-10-03 03:49:23 +0300

    fixup! blossom: Build fastboot custom HAL
    Change-Id: If8b60462306f2f92789f57749e9c6dcb81e2d096
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- device.mk
+++ device.mk
@@ -31,7 +31,7 @@ PRODUCT_PACKAGES += \
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-custom \
+    android.hardware.fastboot@1.1-impl.custom \
     fastbootd
 
 # Health
