952565db508c4902b7faef9c53aede9cafd684ec
Carter Hsu <carterhsu@google.com>
2023-03-15 13:05:55 +0530

    blossom: audio: Update Audio HIDL to 7.1
    Bug: 205235270
    Test: build pass
    Signed-off-by: Carter Hsu <carterhsu@google.com>
    Change-Id: Ie1d8b20b001936e6c72ffe3644475c4fb8dad604

--- device.mk
+++ device.mk
@@ -67,7 +67,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0.vendor \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio@7.0.vendor \
-    android.hardware.audio@7.0-impl \
+    android.hardware.audio@7.1-impl \
     android.hardware.audio@7.0-util.vendor \
     android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.audio@2.0 \
