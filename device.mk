ce9f490aaed3f0371bde2c8c4178fb17158609bb
Ookiineko <chiisaineko@protonmail.com>
2023-07-09 17:26:53 +0800

    blossom: add media libs, configs, seccomp policies and VINTFs
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- device.mk
+++ device.mk
@@ -165,6 +165,15 @@ PRODUCT_PACKAGES += \
     libutils-v30 \
     libutils-v32
 
+# Media
+PRODUCT_PACKAGES += \
+    libavservices_minijail.vendor
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media/,$(TARGET_COPY_OUT_VENDOR)/etc) \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml \
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0.vendor \
@@ -343,6 +352,10 @@ PRODUCT_PACKAGES += \
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/public.libraries.vendor.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/seccomp/,$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy)
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
