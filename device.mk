8d4d776da5b0e97c9748a9fffb872da2613d5fe1
Alcatraz323 <alcatraz32323@gmail.com>
2023-07-19 07:30:27 +0800

    blossom: speed filter to optimize SettingsGoogle

--- device.mk
+++ device.mk
@@ -400,6 +400,9 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-im
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 USE_DEX2OAT_DEBUG := false
 
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SettingsGoogle
+
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
