f0b0f1571ed1bcd497f7665ea8c37567bbdbf826
R0rt1z2 <me@r0rt1z2.com>
2023-10-20 21:01:04 +0200

    blossom: libshim_showlogo: Port to U
    * Updated mangled symbol names for Android 14 and addressed ABI issues related to improper
      return types and handling of the class pointer ('this').
    
    * In createSurface, pass actual parameters instead of null pointers by obtaining the handle
      of the SurfaceControl parent, as referenced in commit 992496bd [1].
    
    * Based on commit a7fe3048 [2], the switch to DisplayMode was primarily a rename; removed
      unnecessary casting and the associated header file.
    
    * Also format the shim with `clang-format`.
    
    - [1]: https://github.com/LineageOS/android_frameworks_native/commit/992496bd2e1af9c93bce33216034dd5419acb444#diff-b8bfdcf33cdb1b09a6a8f0438d5d75cb33d25cba14b276846d8b8ecbd10a70deR1631
    - [2]: https://github.com/LineageOS/android_frameworks_native/commit/a7fe3048aaaf8a45337c7355326f2c6b68b70fb0

--- libshims/libshim_showlogo.cpp
+++ libshims/libshim_showlogo.cpp
@@ -1,35 +1,51 @@
-#include <DisplayConfig.h>
-#include <ui/Size.h>
 #include <gui/SurfaceComposerClient.h>
+#include <ui/Size.h>
 
 using namespace android;
 
 extern "C" {
 
-void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijRKNS_2spINS_7IBinderEEENS_13LayerMetadataEPj(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, const sp<IBinder>& parentHandle, LayerMetadata metadata, uint32_t* outTransformHint);
+void *
+_ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjiiRKNS_2spINS_7IBinderEEENS_3gui13LayerMetadataEPj(
+    void *thisptr, const String8 &name, uint32_t w, uint32_t h,
+    PixelFormat format, uint32_t flags, const sp<IBinder> &parentHandle,
+    LayerMetadata metadata, uint32_t *outTransformHint);
 
-void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataEPj(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, SurfaceControl* parent, LayerMetadata metadata, uint32_t* outTransformHint) {
-  _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijRKNS_2spINS_7IBinderEEENS_13LayerMetadataEPj(name, w, h, format, flags, nullptr, metadata, nullptr);
- }
-
-void _ZN7android14SurfaceControl10getSurfaceEv(void);
-
-void _ZNK7android14SurfaceControl10getSurfaceEv(void) {
-  _ZN7android14SurfaceControl10getSurfaceEv();
- }
-
-status_t _ZN7android21SurfaceComposerClient20getActiveDisplayModeERKNS_2spINS_7IBinderEEEPNS_2ui11DisplayModeE(const sp<IBinder>& display, ui::DisplayMode* mode);
-
-status_t _ZN7android21SurfaceComposerClient22getActiveDisplayConfigERKNS_2spINS_7IBinderEEEPNS_13DisplayConfigE(const sp<IBinder>& display, DisplayConfig* config) {
-  return _ZN7android21SurfaceComposerClient20getActiveDisplayModeERKNS_2spINS_7IBinderEEEPNS_2ui11DisplayModeE(display, (ui::DisplayMode*)config);
+void *
+_ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataEPj(
+    void *thisptr, const String8 &name, uint32_t w, uint32_t h,
+    PixelFormat format, uint32_t flags, SurfaceControl *parent,
+    LayerMetadata metadata, uint32_t *outTransformHint) {
+  return _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjiiRKNS_2spINS_7IBinderEEENS_3gui13LayerMetadataEPj(
+      thisptr, name, w, h, format, flags, parent->getHandle(), metadata, outTransformHint);
 }
 
-status_t _ZN7android21SurfaceComposerClient11Transaction5applyEbb(void* thisptr, bool synchronous, bool oneWay);
+void *_ZN7android14SurfaceControl10getSurfaceEv(void *thisptr);
 
-status_t _ZN7android21SurfaceComposerClient11Transaction5applyEb(void* thisptr, bool synchronous) {
-    return _ZN7android21SurfaceComposerClient11Transaction5applyEbb(thisptr, synchronous, false /* oneWay */);
+void *_ZNK7android14SurfaceControl10getSurfaceEv(void *thisptr) {
+  return _ZN7android14SurfaceControl10getSurfaceEv(thisptr);
+}
+
+status_t
+_ZN7android21SurfaceComposerClient20getActiveDisplayModeERKNS_2spINS_7IBinderEEEPNS_2ui11DisplayModeE(
+    void *thisptr, const sp<IBinder> &display, ui::DisplayMode *mode);
+
+status_t
+_ZN7android21SurfaceComposerClient22getActiveDisplayConfigERKNS_2spINS_7IBinderEEEPNS_13DisplayConfigE(
+    void *thisptr, const sp<IBinder> &display, ui::DisplayMode *config) {
+  return _ZN7android21SurfaceComposerClient20getActiveDisplayModeERKNS_2spINS_7IBinderEEEPNS_2ui11DisplayModeE(
+      thisptr, display, config);
+}
+
+status_t _ZN7android21SurfaceComposerClient11Transaction5applyEbb(
+    void *thisptr, bool synchronous, bool oneWay);
+
+status_t
+_ZN7android21SurfaceComposerClient11Transaction5applyEb(void *thisptr,
+                                                        bool synchronous) {
+  return _ZN7android21SurfaceComposerClient11Transaction5applyEbb(
+      thisptr, synchronous, false /* oneWay */);
 }
 
 ui::Size _ZN7android2ui4Size7INVALIDE;
-
 }
