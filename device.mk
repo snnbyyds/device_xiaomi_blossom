8b6261fccbd4831e4463d86dc63d22f2b501d289
Sushrut1101 <guptasushrut@gmail.com>
2023-11-01 11:33:47 +0530

    blossom: Link sensor blobs against HIDL `libsensorndkbridge`
    * Import and rebrand vndk 33 libsensorndkbridge to
      libsensorndkbridge-hidl
    
    * https://android.googlesource.com/platform/frameworks/hardware/interfaces/+/refs/tags/android-13.0.0_r75/sensorservice/libsensorndkbridge
    
    Change-Id: Id774449827725d5998a00abfba3fc7d4bf6ed16f
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -375,7 +375,8 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     android.hardware.sensors@1.0.vendor \
     android.hardware.sensors@2.0.vendor \
-    libsensorndkbridge
+    libsensorndkbridge \
+    libsensorndkbridge-hidl
 
 # GNSS
 PRODUCT_PACKAGES += \
--- extract-files.sh
+++ extract-files.sh
@@ -59,6 +59,10 @@ function blob_fixup() {
             grep -q "libprotobuf-cpp-lite-3.9.1.so" "${2}" && \
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
             ;;
+        vendor/bin/mnld | vendor/lib*/libaalservice.so | vendor/lib*/libcam.utils.sensorprovider.so)
+            grep -q "libsensorndkbridge.so" "${2}" && \
+            "${PATCHELF}" --replace-needed "libsensorndkbridge.so" "libsensorndkbridge-hidl.so" "${2}"
+            ;;
     esac
 }
 
